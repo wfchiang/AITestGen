be0121abdf1a44851e56c4239757619ad911c7ce
wfchiang <weifan.wf@gmail.com>
2023-06-14 17:08:55 -0400

    an another initial check-in

--- src/aitestgen/ir/node.py
+++ src/aitestgen/ir/node.py
@@ -1,4 +1,9 @@
+from typing import List 
 import ast 
+
+# ====
+# Class definition
+# ====
 
 class Expression : 
     def __init__ (self): 
@@ -30,3 +35,45 @@ class BinaryExpression (Expression):
         self.lhs = lhs 
         self.rhs = rhs 
 
+class TestGenerationContext : 
+    def __init__(self) -> None:
+        self.variables = [] 
+        self.conditions = [] 
+
+    def add_variable (self, var :Variable): 
+        assert(isinstance(var, StrVariable)), 'Error: currently, we only support string-variables' 
+        assert(all([var.name != v.name for v in self.variables])), 'Error: duplicated variable name: {}'.format(var.name)
+        self.variables.append(var) 
+
+    def add_variables (self, vars :List[Variable]): 
+        assert(isinstance(vars, List))
+        for v in vars: 
+            self.add_variable(v) 
+
+    def add_condition (self, cond): 
+        self.conditions.append(cond) 
+
+    def add_conditions (self, conds):
+        assert(isinstance(conds, List)) 
+        for c in conds: 
+            self.add_condition(c) 
+
+    def clone (self): 
+        # init my clone 
+        my_clone = TestGenerationContext() 
+
+        # setup the variables 
+        my_clone.add_variables(self.variables) 
+
+        # setup the conditions 
+        my_clone.add_conditions(self.conditions)
+
+        # return 
+        return my_clone
+
+# ====
+# Node generation functions 
+# ====
+def create_string_variable_from_arg (ast_arg :ast.arg): 
+    assert(isinstance(ast_arg, ast.arg)) 
+    return StrVariable(ast_arg.arg) 
--- src/aitestgen/parser/python_parser.py
+++ src/aitestgen/parser/python_parser.py
@@ -1,5 +1,8 @@
 import ast, inspect
+from typing import List 
 from functools import wraps 
+
+from ..ir import node as ir_node 
 
 # ====
 # Decorator 
@@ -10,7 +13,7 @@ def analyze ():
         print("Analyzing function {}".format(f)) 
 
         f_ast_node = ast.parse(inspect.getsource(f)) 
-        parse_ast_node(f_ast_node)
+        tg_contexts = parse_ast_node_for_test_generation_contexts(f_ast_node)
 
         # Wrap the original function "f" and return 
         @wraps(f) 
@@ -31,18 +34,56 @@ class UnhandledScenarioException (Exception):
 # ====
 # Parser procedure
 # ====
-def parse_ast_node (ast_node): 
+def parse_ast_statement_for_test_generation_conditions (ast_node): 
+    return [] 
+
+def parse_ast_node_for_test_generation_contexts (ast_node): 
     if (isinstance(ast_node, ast.Module)): 
         module_body = ast_node.body 
         
         if (len(module_body) == 1): 
-            return parse_ast_node(module_body[0])
+            return parse_ast_node_for_test_generation_contexts(module_body[0])
 
         else: 
             raise UnhandledScenarioException("Can only handle the case of function-def -- the first and the only one under the Module object")
         
     elif (isinstance(ast_node, ast.FunctionDef)): 
-        pass 
+        # Grab the function arguments 
+        func_args = list(map(lambda arg: ir_node.create_string_variable_from_arg(arg), ast_node.args.args)) 
+
+        # Create a test-generation context 
+        tg_context = ir_node.TestGenerationContext() 
+        tg_context.add_variables(func_args) 
+
+        test_generation_contexts = [tg_context] 
+        
+        # Loop through the function body to generate conditions 
+        for f_body_stmt in ast_node.body: 
+            conds = parse_ast_statement_for_test_generation_conditions(f_body_stmt) 
+            assert(isinstance(conds, List)) 
+
+            if (len(conds) == 0): 
+                pass 
+
+            elif (len(conds) == 1): 
+                test_generation_contexts[-1].add_condition(conds[0])
+
+            elif (len(conds) == 2): 
+                tg_contexts_0 = [tgc.clone() for tgc in test_generation_contexts]
+                tg_contexts_1 = [tgc.clone() for tgc in test_generation_contexts] 
+
+                for tgc in tg_contexts_0: 
+                    tgc.add_condition(conds[0])
+                
+                for tgc in tg_contexts_1: 
+                    tgc.add_condition(conds[1]) 
+
+                test_generation_contexts = tg_contexts_0 + tg_contexts_1
+
+            else: 
+                raise UnhandledScenarioException("A statement should generate at most 2 conditions...")
+
+        print(func_args)
 
     else: 
         raise UnhandledScenarioException("Unhandled AST node type: {} : {}".format(type(ast_node), ast.dump(ast_node, indent=4)))
